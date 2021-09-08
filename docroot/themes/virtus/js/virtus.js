e13d2e2765565466e6d92a111b48c320b1db5ed1
julian.barrios <julian.barrios@jerrejerre.com>
2021-09-08 11:47:10 -0500

    feat(form) currently dev on form

---
+++ docroot/themes/virtus/js/virtus.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * virtus behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.virtus = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
